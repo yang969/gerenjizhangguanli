e7a49146040ff2d57a8a47f864e9ee92e232ca4e
baiyang1024 <52342795+baiyang1024@users.noreply.github.com>
2019-06-30 22:44:39 +0800

    Update UserServiceImpl.java

--- UserServiceImpl.java
+++ UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Service("userService")
+@Service("userService")                  
 public class UserServiceImpl implements UserService {
 
     @Resource
