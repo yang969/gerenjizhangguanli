ae2484e578945278f7c16fb73d14350cb92077a0
baiyang1024 <52342795+baiyang1024@users.noreply.github.com>
2019-06-30 22:30:43 +0800

    Update RoleServiceImpl.java

--- RoleServiceImpl.java
+++ RoleServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
-
+               
 @Service("roleService")
 public class RoleServiceImpl implements RoleService {
 
