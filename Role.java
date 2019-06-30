21c8b40044ba1640deebc6fdefbfae480d1254b1
baiyang1024 <52342795+baiyang1024@users.noreply.github.com>
2019-06-30 22:43:23 +0800

    Update Role.java

--- Role.java
+++ Role.java
@@ -22,7 +22,7 @@ public class Role {
     }  
                                                                             
     public void setId(Integer id) {
-        this.id = id;
+        this.id = id;               
     }
 
     public String getRoleName() {
